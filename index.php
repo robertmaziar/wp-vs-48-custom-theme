68b8b2e7bd1114063ab355ebb70f790317a02ae4
Robert Maziar <robert.maziar@gmail.com>
2017-08-14 21:38:11 -0400

    Cleaned up pages and added author links.

--- index.php
+++ index.php
@@ -6,6 +6,25 @@ if (have_posts()) :
 	while (have_posts()) : the_post(); ?>
 		<article class="post">
 			<h2><a href="<?php  the_permalink() ?>"><?php the_title() ?></a></h2>
+
+			<p class="post-info">
+				<?php the_time('F jS, Y'); ?> | By
+				<a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> | Posted in
+				<?php 
+					$categories = get_the_category();
+					$seperator = ", ";
+					$output = '';
+
+					if ($categories) {
+						foreach($categories as $category) {
+							$output .= '<a href="' . get_category_link($category->term_id) .'">' . $category->cat_name . '</a>' . $seperator; 
+						}
+
+						echo trim($output, $seperator);
+					}
+				?>
+			</p>
+
 			<p><?php the_content() ?></p>
 		</article>
 
